d34ba21bb69522c0b58d007a2ed04f3f93a23f3b
NgJoeYi <s10262850@connect.np.edu.sg>
2023-11-18 18:51:55 +0800

    FED_ASSGN1

--- MrsBeanCafe.js
+++ MrsBeanCafe.js
@@ -1,18 +1,68 @@
-// When the DOM is fully loaded, execute the following code
-document.addEventListener('DOMContentLoaded', function () {
-    // Get references to important elements
+document.addEventListener("DOMContentLoaded", function () {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    setInterval(function () {
+        plusSlides(1);
+    }, 3000);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n, event) {
+        event.preventDefault(); // Prevent the default behavior of the anchor tag
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot-homepage");
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        for (i = 0; dots && i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        if (slides[slideIndex - 1] && dots[slideIndex - 1]) {
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    }
+
+    // Attach click event listeners to dots
+    let dotElements = document.getElementsByClassName("dot-homepage");
+    for (let i = 0; i < dotElements.length; i++) {
+        dotElements[i].addEventListener("click", function (event) {
+            currentSlide(i + 1, event);
+        });
+    }
+
+    // Get references to important elements for the shopping cart
     const cartContainer = document.getElementById('cart-container');
     const cart = document.getElementById('cart');
     const closeCart = document.getElementById('close-cart');
     const cartItems = document.getElementById('cart-items');
     const checkoutButton = document.getElementById('checkout-button');
 
-    // ------------------------------------------------------- Add click event to close the cart
+    // Add click event to close the cart
     closeCart.addEventListener('click', function () {
         closeCartMenu(); // Call the function to close the cart
     });
 
-    // -------------------------------------------------------------------- Add item to the cart
+    // Add item to the cart
     const addItemToCart = function (itemName, price) {
         // Create a new cart item element
         const cartItem = document.createElement('div');
@@ -22,18 +72,18 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleCart(); // Show the cart
     };
 
-    // ------------------------------------------------------------------- Toggle cart visibility
+    // Toggle cart visibility
     const toggleCart = function () {
         // Toggle the cart visibility by changing its right style property
         cartContainer.style.right = cartContainer.style.right === '0px' ? '-300px' : '0';
     };
 
-    // --------------------------------------------------------------------------- Close the cart
+    // Close the cart
     const closeCartMenu = function () {
         cartContainer.style.right = '-300px'; // Hide the cart by moving it off-screen
     };
 
-    // ------------------------------------------------ Add click event listeners to all cart logos
+    // Add click event listeners to all cart logos
     const cartLogos = document.querySelectorAll('.cart-logo');
     cartLogos.forEach(function (cartLogo) {
         // When a cart logo is clicked, add the corresponding item to the cart
@@ -44,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // ----------------------------------------------------------------------------- Checkout button
+    // Checkout button
     checkoutButton.addEventListener('click', function () {
         alert('Order received'); // Show an alert when the checkout button is clicked
     });
